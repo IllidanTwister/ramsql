cfc59bfa7e4326018283938698cc4c8f26540f7b
angemili <angemili@tencent.com>
2021-07-28 16:50:30 +0800

    feat: support time format 2006-01-02 15:04:05.999999999

--- engine/parser/date.go
+++ engine/parser/date.go
@@ -13,6 +13,8 @@ const DateShortFormat = "2006-Jan-02"
 
 const DateNumberFormat = "2006-01-02"
 
+const DateNumberFormatWithDetailTime = "2006-01-02 15:04:05.999999999"
+
 // ParseDate intends to parse all SQL date format
 func ParseDate(data string) (*time.Time, error) {
 	t, err := time.Parse(DateLongFormat, data)
@@ -31,6 +33,11 @@ func ParseDate(data string) (*time.Time, error) {
 	}
 
 	t, err = time.Parse(DateNumberFormat, data)
+	if err == nil {
+		return &t, nil
+	}
+
+	t, err = time.Parse(DateNumberFormatWithDetailTime, data)
 	if err == nil {
 		return &t, nil
 	}
